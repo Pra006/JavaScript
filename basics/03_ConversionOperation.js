d74c37d0a88af7b957104a702d3b9749f1e25e8f
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-06-18 16:34:15 +0000

    conversionoperation

---
+++ basics/03_ConversionOperation.js
@@ -0,0 +1,23 @@
+let Score = "Prakash"
+
+//console.log(typeof Score);
+let ValueInNumber= Number(Score)
+//console.log(typeof Score)
+//console.log(ValueInNumber)
+
+//"33"=> 33
+//"33abc"=> Nan
+//true => 1;
+//False => 0;
+
+//let isLoggedIn = 0
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true;  0=> false
+//"Prakash" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
