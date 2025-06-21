cbf1a44b462c171ade4c2dfccab7e74d1b45e670
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-06-21 16:39:36 +0000

    datatype summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,14 @@
+// Primitive
+ //7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+ // Non-Primitive(call by reference): Array, Objects, Functions 
+
+ const a =["Aakash","Prakash", "Ankit"]
+ 
+ let myObj={
+    name:"Prakash",
+    age: 22,
+ }
+ const myFunction= function(){
+    console.log("Prakash"); 
+ }
