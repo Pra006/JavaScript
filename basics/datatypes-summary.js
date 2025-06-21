29b30acccbc0d11675c3bd545b36ba574ca55776
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-06-21 17:11:09 +0000

    stack, heap

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -10,5 +10,26 @@
     age: 22,
  }
  const myFunction= function(){
-    console.log("Prakash"); 
+    // console.log("Prakash"); 
  }
+
+//++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive), Heap(Non-Primitive)
+let fruits= "apple"
+let mango = fruits
+mango = "banana"
+// console.log(mango);
+// console.log(mango);
+
+
+let userone={
+    email:"user@gmail.com",
+    upi: "user@byl"
+}
+let usertwo= userone
+usertwo.email = "user@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
+
