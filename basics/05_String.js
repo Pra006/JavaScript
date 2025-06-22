62c4af5c156e747050a4a55f3baed050633b16bf
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-06-22 03:45:38 +0000

    number

---
+++ basics/05_String.js
@@ -0,0 +1,42 @@
+const name = "Prakash"
+const age = 22
+
+// console.log(name + age );
+
+// console.log(`hello my name is${name} my age is${age}`)
+
+const gameName = new String('prakash')
+
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('k'));
+// console.log(gameName.endsWith(7));
+
+const newString = gameName.substring(-8,4);
+// console.log(newString);
+
+const anotherString = gameName.split("");
+// console.log(anotherString);
+
+const sameString = gameName.slice(-7,3);
+// console.log(sameString);
+
+const greetings = "    Hello world     ";
+console.log(greetings);
+console.log(greetings.trim());
+console.log(greetings.trimEnd());
+console.log(greetings.trimRight());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ basics/06_number.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(300);
+console.log(balance);
+console.log(balance.toFixed(2));
+console.log(balance.toString().concat(200));
+
+const number = 123.8766
+console.log(number.toPrecision(3));
+
+
+const hundred = 1000000000
+console.log(hundred.toLocaleString("en-IN"));
