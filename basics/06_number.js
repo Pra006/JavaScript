fcf8f17624165fd66ec819e8a3d4988629bcddd1
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-06-24 14:20:06 +0000

    number

--- basics/06_number.js
+++ basics/06_number.js
@@ -1,14 +1,28 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(300);
-console.log(balance);
-console.log(balance.toFixed(2));
-console.log(balance.toString().concat(200));
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().concat(200));
 
 const number = 123.8766
-console.log(number.toPrecision(3));
+// console.log(number.toPrecision(3));
 
 
 const hundred = 1000000000
-console.log(hundred.toLocaleString("en-IN"));
+// console.log(hundred.toLocaleString("en-IN"));
+
+//*********Maths******************* */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(5.3));
+console.log(Math.min(4,5,6,7,8,2));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
