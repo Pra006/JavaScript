f015bf7d8ee1364b78214961964bcd9fd8e553fa
Pra006 <104180826+Pra006@users.noreply.github.com>
2025-07-13 08:02:57 +0000

    forloops

---
+++ 05_Loops/forloop.js
@@ -0,0 +1,37 @@
+
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element ==2)
+    {
+       // console.log("2 is the best numebr");
+    }
+    //console.log(element);
+// }
+//  for (let i = 0; i < 10; i++) {
+//     console.log(`outer loop : ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//       //  console.log(`inner loop : ${j} and inner loop ${i}`);
+//          console.log(i+ '*'+ j + '=' + i*j);
+         
+//     }
+    
+ }
+ let myArray = ["Flash", "Superman", "Batman",]
+//  console.log(myArray.length);
+//  for (let index = 0; index < myArray.length; index++) {
+//    const element = myArray[index];
+//    console.log(element);
+//  }
+
+//break and continue
+for (let index = 0; index <= 10; index++) {
+   if (index == 5)
+   {
+      console.log(`Detected 5`);
+      continue
+   }
+   console.log(`value of index is ${index}`);
+}
+
+
